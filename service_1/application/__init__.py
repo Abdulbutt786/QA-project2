e4a4f20ca586fdb2b5c307d5bb8526451fb1432c
Abdul Butt <abdulbutt123@gmail.com>
2022-06-24 13:06:25 +0000

    initial commit

---
+++ service_1/application/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+import requests
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
+app.config['SECRET_KEY'] = str(os.getenv('MY_SECRET_KEY'))
+db = SQLAlchemy(app)
+
+
+from application import routes
