f1a35e7bd70638022d85433d7ccc0de62431f28d
Abdul Butt <abdulbutt123@gmail.com>
2022-06-26 12:44:48 +0000

    added templates and some routes

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -1,1 +1,15 @@
-service 1 created branch 
+from application import app
+from flask import render_template
+import requests
+
+@app.route('/')
+@app.route('/home')
+def index():
+	return render_template('index.html')
+
+@app.route('/generate')
+def index():
+    breakfast = requests.get('http://service2:5000/get_breakfast').text
+    lunch = requests.get('http://service3:5000/get_lunch').text
+    price = requests.post('http://service4:5000/price', json=dict(chars=chars, nums=nums))
+    return render_template('home.html', prize = prize.text)
