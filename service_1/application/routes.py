dda387c6df52ed0eb80e6d6e9978e482a17d03c3
Abdul Butt <abdulbutt123@gmail.com>
2022-06-26 13:20:19 +0000

    added a databasebase stuff

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -1,15 +1,23 @@
 from application import app
 from flask import render_template
+from application import app, db
 import requests
+from application.models import Meal
 
 @app.route('/')
-@app.route('/home')
+@app.route('/')
 def index():
 	return render_template('index.html')
 
 @app.route('/generate')
-def index():
-    breakfast = requests.get('http://service2:5000/get_breakfast').text
-    lunch = requests.get('http://service3:5000/get_lunch').text
-    price = requests.post('http://service4:5000/price', json=dict(chars=chars, nums=nums))
-    return render_template('home.html', prize = prize.text)
+def home():
+    breakfast = requests.get('http://service2:5000/get_breakfast')
+    lunch = requests.get('http://service3:5000/get_lunch', data=breakfast.text)
+    price = requests.post('http://service4:5000/price',data=lunch.txt )
+
+    db_info = Meal(breakfast=breakfast.text, lunch=lunch.text, price=price.text)
+	db.session.add(meal_db)
+	db.session.commit()
+	recent = Meal.query.first()
+
+    return render_template('home.html', breakfast=
