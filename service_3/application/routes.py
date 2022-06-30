32058000e0afacb9bb4ddc62bda23112144abac5
Abdul Butt <abdulbutt123@gmail.com>
2022-06-30 19:13:59 +0000

    debug changes to routes

--- service_3/application/routes.py
+++ service_3/application/routes.py
@@ -3,7 +3,7 @@ import random
 
 @app.route('/get_lunch', methods=['GET'])
 def get_lunch():
-    lunches = ['Veg Pizza', 'Chicken and chips', 'Chicken fried rice', 'Burger and Chips', 'Pa', 
+    lunches = ['Veg Pizza', 'Chicken and chips', 'Chicken fried rice', 'Burger and Chips', 'Pasta', 
     'Lentil soup', 'Mac and Cheese', 'Beef burger', 'Grilled Cheese', 'Ramen noodles' ]
     lunch = random.choice(lunches)
     return lunch
