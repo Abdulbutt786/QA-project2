63070ab71a74185d9a8bc7f8bf107c8a6a2d6d07
Abdul Butt <abdulbutt123@gmail.com>
2022-06-30 16:28:48 +0000

    added routes for service 3

--- service_3/application/routes.py
+++ service_3/application/routes.py
@@ -3,7 +3,7 @@ import random
 
 @app.route('/get_lunch', methods=['GET'])
 def get_lunch():
-    lunches = ['Pizza', 'Chicken and chips', '', '', '', 
-    '', '', '', '', '' ]
+    lunches = ['Veg Pizza', 'Chicken and chips', 'Chicken fried rice', 'Burger and Chips', 'Pa', 
+    'Lentil soup', 'Mac and Cheese', 'Beef burger', 'Grilled Cheese', 'Ramen noodles' ]
     lunch = random.choice(lunches)
     return lunch
