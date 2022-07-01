b3647ae3a0eaf92166cc64870bd7cb48071d7572
Abdul Butt <abdulbutt123@gmail.com>
2022-07-01 09:11:44 +0000

    made changes to routes

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -1,19 +1,10 @@
 from application import app
 from flask import render_template
-from application import app, db
 import requests
-from application.models import Meal
 
-@app.route('/')
 @app.route('/')
 def index():
-	return render_template('index.html')
-
-@app.route('/')
-def home():
-    breakfast = requests.get('http://service2:5000/get/breakfast')
-    lunch = requests.get('http://service3:5000/get/lunch', data=breakfast.text)
-    calories = requests.post('http://service4:5000/calories',data=lunch.txt )
-
-
-    return render_template('home.html', breakfast=
+    breakfast = requests.get('http://service2:5000/get_breakfast').text
+    lunch = requests.get('http://service3:5000/get_lunch').text
+    calories = requests.post('http://service4:5000/calories',json={''})
+    return render_template('home.html', )
