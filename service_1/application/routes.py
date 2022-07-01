051ff734aecd390416c3ffe101c7beb0ecc8b76e
Abdul Butt <abdulbutt123@gmail.com>
2022-07-01 10:27:22 +0000

    updated services

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -6,5 +6,5 @@ import requests
 def index():
     breakfast = requests.get('http://service2:5000/get_breakfast').text
     lunch = requests.get('http://service3:5000/get_lunch').text
-    calories = requests.post('http://service4:5000/calories',json={''})
+    calories = requests.post('http://service4:5000/calories',json={'breakfast':'breakfasts','get_lunch':'lunches'})
     return render_template('home.html', )
