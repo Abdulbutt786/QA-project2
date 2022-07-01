96e2e8ffa5b5ca4e5aba79441eb568b98be07e8e
Abdul Butt <abdulbutt123@gmail.com>
2022-07-01 12:24:55 +0000

    made changes to routes

--- service_1/application/__init__.py
+++ service_1/application/__init__.py
@@ -1,13 +1,5 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-import os
-import requests
+
 app = Flask(__name__)
 
-
-app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
-app.config['SECRET_KEY'] = str(os.getenv('MY_SECRET_KEY'))
-db = SQLAlchemy(app)
-
-
-from application import routes
+import application.routes
