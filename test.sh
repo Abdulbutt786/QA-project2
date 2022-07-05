7b9c8746d35108a4f3e6e85bc499342821d2d913
Abdulbutt <abdulbutt123@gmail.com>
2022-07-05 11:55:07 +0000

    changed

--- test.sh
+++ test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-python3 -m venv venv
-
-sudo apt install python3.8-venv
-
-source venv/bin/activate
-
-pip3 install -r requirements.txt
-
-python3 -m pytest --cov=. --cov-report xml -v
+declare -a directories=("service_1" "service_2" "service_3" "service_4")
+for dir in "${directories[@]}"
+do
+  cd ${dir}
+  sudo apt update
+  sudo apt install python3 python3-pip python3.8-venv
+  python3 -m venv venv
+  source venv/bin/activate
+  pip3 install -r requirements.txt
+  python3 -m pytest --cov --cov-report xml -v
+  deactivate
+  cd ..
+done
